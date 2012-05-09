70dc4d24710de454ecbedd8530a93735edc6246b
Matthew Jacobi <matt@oppian.com>
2012-05-09 14:16:31 +0100

    Faster solution for largest_sum_non_adj

--- src/interview_questions/largest_sum_non_adj.py
+++ src/interview_questions/largest_sum_non_adj.py
@@ -1,25 +1,111 @@
 '''
-Created on May 8, 2012
 
-@author: matt
+Question
+========
 
 You are given an array full of positive integers. Write a function that returns
 the largest sum you can get by adding together numbers in non-adjacent indices
 from the array. I.e. you if you include the things stored in arr[i] in your sum,
 you can't include what is stored in arr[i-1] or arr[i+1]
 
-TODO:
-ways to optimize could be to remember ones we have added before for gaps and indexes that we have added before
+Examples
+========
+
+A = [1, 2, 3]
+
+Should return 4 which can be found from:
+
+[1, 0, 3]
+
+Another more complex example:
+
+Given A = [1, 2, 100, 4, 5, 100, 7, 8, 100, 10]
+
+It should return 301 which is comprised of:
+
+[1, 0, 100, 0, 0, 100, 0, 0, 100,  0]
+
+Solution
+========
+
+Current solution is to calculate the number of permutations using gaps of 2 or 3
+(any more will be included in those series) Gaps of 4 can have one in the middle
+so we discount those and gaps of 5 can be a gap of 2 and 3 so discount those.
+Gaps of 6 are 2 gaps of 3 etc.
+
+So example:
+
+[1, 2, 100, 4, 5, 100, 7, 8, 100, 10]
+
+We have the following permutations:
+
+[1, 0, 100, 0, 5,   0, 7, 0, 100,  0] sum = 213
+[1, 0, 100, 0, 5,   0, 7, 0,   0, 10] sum = 123
+[1, 0, 100, 0, 5,   0, 0, 8,   0, 10] sum = 124
+[1, 0, 100, 0, 0, 100, 0, 8,   0, 10] sum = 219
+[1, 0, 100, 0, 0, 100, 0, 0, 100,  0] sum = 301
+[1, 0,   0, 4, 0, 100, 0, 8,   0, 10] sum = 123
+[1, 0,   0, 4, 0, 100, 0, 0, 100,  0] sum = 205
+[1, 0,   0, 4, 0,   0, 7, 0, 100,  0] sum = 112
+[1, 0,   0, 4, 0,   0, 7, 0,   0, 10] sum =  22
+[0, 2,   0, 4, 0, 100, 0, 8,   0, 10] sum = 124
+[0, 2,   0, 4, 0, 100, 0, 0, 100,  0] sum = 206
+[0, 2,   0, 4, 0,   0, 7, 0, 100,  0] sum = 113
+[0, 2,   0, 4, 0,   0, 7, 0,   0, 10] sum =  23
+[0, 2,   0, 0, 5,   0, 7, 0, 100,  0] sum = 114
+[0, 2,   0, 0, 5,   0, 7, 0,   0, 10] sum =  24
+[0, 2,   0, 0, 5,   0, 0, 8,   0, 10] sum =  25
+
+The largest being 301
 
 '''
-
 def largest_sum_non_adj(A):
     '''
-    we want to try every 2nd or 3rd (and every combinbation of those)
-    since trying every 4th means we could do the 2nds in between
-    and every gap of N can be filled with either a 2 or 3 gap
-    eg a gap of 5 is a 2 and a 3
+    This algorithm works out the number of permutations the list can have using
+    gaps of 2 or 3. Uses more memory than naive but is a lot faster.
     '''
+    maximum_number_of_items = len(A)
+    
+    maximum = 0
+    if maximum_number_of_items == 0:
+        return maximum
+    
+    stack = [[0], [A[0]]]
+    
+    while len(stack) > 0:
+        current = stack.pop()
+        while len(current) < maximum_number_of_items:
+            
+            current_length = len(current)
+            
+            # default is to add a zero
+            next_item = 0
+            
+            if current[-1] == 0:
+                # we had a zero at the end of the currently considered items
+                next_item = A[current_length]
+                if current_length > 1 and current[-2] != 0 and current_length < maximum_number_of_items - 1:
+                    # we put the zero option on the stack to evaluate later
+                    other_option = current[:]
+                    other_option.append(0)
+                    stack.append(other_option)
+                        
+            current.append(next_item)
+        
+        maximum = max(maximum, sum(current))
+        
+    return maximum
+
+
+
+def largest_sum_non_adj_naive(A):
+    """
+    we want to try every 2nd or 3rd (and every combinbation of those) since
+    trying every 4th means we could do the 2nds in between and every gap of N
+    can be filled with either a 2 or 3 gap eg a gap of 5 is a 2 and a 3
+    
+    While naive and slow, it uses less memory (n/2) array of gaps
+    """
     maximum = 0
     # create an array to keep track of gaps
     gap_array = [2] * (len(A) / 2)
@@ -36,8 +122,9 @@ def largest_sum_non_adj(A):
 def inc_gaps(gaps, overflow=3):
     '''
     Gaps is a list with only 2 possible values (so like a list of bits) but the
-    values are 2 and 3. When you increment from 3 it goes to 2 with a carry to the next.
-    If the carry overflows to the end, we have reached the end so zero out the list.
+    values are 2 and 3. When you increment from 3 it goes to 2 with a carry to
+    the next. If the carry overflows to the end, we have reached the end so zero
+    out the list.
     '''
     index = 0
     gaps[index] += 1
@@ -77,12 +164,30 @@ def find_sum(A, gaps, offset=0):
     return sum
         
 
+from random import randrange
+import time
 import unittest
-
 
 class Test(unittest.TestCase):
 
 
+    def do_random_test(self, length):
+        A = [randrange(1, 1000000) for n in xrange(length)]; n = n
+
+        start_time = time.time()
+        answer = largest_sum_non_adj(A)
+        calc_time = time.time() - start_time
+        
+        
+        start_time = time.time()
+        naive_answer = largest_sum_non_adj_naive(A)
+        naive_answer = answer
+        naive_time = time.time() - start_time
+        
+        print "random test of length (%d) - naive time = %.3f, calc time = %.3f" % (length, naive_time, calc_time)
+        self.assertEquals(naive_answer, answer)
+        
+        
     def testSimple(self):
         A = [1, 2, 3]
         self.assertEquals(4, largest_sum_non_adj(A))
@@ -100,6 +205,19 @@ class Test(unittest.TestCase):
         self.assertEquals(41, largest_sum_non_adj(A))
         A = [1, 2, 100, 4, 5, 100, 7, 8, 100, 10]
         self.assertEquals(301, largest_sum_non_adj(A))
+    
+    def testRandom(self):  
+        self.do_random_test(10)
+        self.do_random_test(20)
+        self.do_random_test(30)
+        self.do_random_test(40)
+        
+ 
+    def testPerms(self):
+        size = 30
+        range = [x for x in xrange(1, size + 1)]
+        for i in xrange(size):
+            largest_sum_non_adj(range[:i])
         
 
 if __name__ == "__main__":
